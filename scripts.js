a05359c5d2a4e7f7f1d346f8b2ef873879df55fc
NachoFLizaur <nachetefl@gmail.com>
2020-05-15 14:09:36 +0200

    added function to read input from operation table

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function fetchdata() {
+function fetch_data() {
     // Create a request variable and assign a new XMLHttpRequest object to it.
     var request = new XMLHttpRequest()
 
@@ -16,7 +16,7 @@ function fetchdata() {
             document.getElementById("curr_btc").value = data.last;
             document.getElementById("min_buy").value = document.getElementById("curr_btc").value * 0.99025;
 
-            loadnums(document.getElementById("btc_balance").value, document.getElementById("usd_balance").value)
+            load_nums(document.getElementById("btc_balance").value, document.getElementById("usd_balance").value)
         } else {
             console.log('error')
         }
@@ -26,7 +26,7 @@ function fetchdata() {
     request.send()
 }
 
-function loadnums(btc, usd) {
+function load_nums(btc, usd) {
     console.log("TEST");
     console.log(btc);
     console.log(usd);
@@ -34,12 +34,42 @@ function loadnums(btc, usd) {
     document.getElementById("btc_sell").value = btc * 0.995 * curr_btc;
 }
 
-function addrow() {
-    var tablaExpandible = document.getElementById("per_calc");
+function add_row() {
+    var tablaExpandible = document.getElementById("per_calc_table");
     var fila = tablaExpandible.insertRow(tablaExpandible.rows.length - 1);
     var col1 = fila.insertCell(0);
     var col2 = fila.insertCell(1);
     var form = "<input type='text'>"
     col1.innerHTML = form;
     col2.innerHTML = form;
+}
+
+function calc_revenue() {
+    var table = document.getElementById("per_calc_table");
+    var op_amount = [];
+    var op_buy_price = [];
+
+    for (var i = 0; i < table.rows.length - 1; i++) {
+        row = table.rows[i];
+        // val1 = row.cells[0].getElementById("input1").value;
+        // val2 = row.cells[1];
+        amount = $(row.cells[0]).find("input").val();
+        buy_price = $(row.cells[1]).find("input").val();
+
+        op_amount.push(amount);
+        op_buy_price.push(buy_price);
+
+        console.log("amount")
+        console.log(amount)
+        console.log("buy_price")
+        console.log(buy_price)
+        
+    }
+
+    console.log("Array prints")
+    for(var i = 0; i < op_amount.length; i++){
+        console.log(op_amount[i]);
+        console.log(op_buy_price[i]);
+    }
+
 }
