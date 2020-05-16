58baeff1491dd1a9238203db32a5730926105dda
NachoFLizaur <nachetefl@gmail.com>
2020-05-16 11:06:43 +0200

    improved html structure. Added dual currency

--- scripts.js
+++ scripts.js
@@ -1,42 +1,63 @@
 function fetch_data() {
-    // Create a request variable and assign a new XMLHttpRequest object to it.
+    // Create a request_usd variable and assign a new XMLHttprequest object to it.
 
-    var request = new XMLHttpRequest()
+    var request_usd = new XMLHttpRequest()
+    var request_eur = new XMLHttpRequest()
     
-    // Open a new connection, using the GET request on the URL endpoint
-    // request.open('GET', 'https://www.bitstamp.net/api/ticker/', true)
+    // Open a new connection, using the GET request_usd on the URL endpoint
     
-    request.onload = function () {
+    request_usd.onload = function () {
         // Begin accessing JSON data here
         var data = JSON.parse(this.response)
         
-        if (request.status >= 200 && request.status < 400) {
+        if (request_usd.status >= 200 && request_usd.status < 400) {
             var data = JSON.parse(this.response)
-            console.log("btc_price")
+            console.log("btc_price_usd")
             console.log(data.last)
-            document.getElementById("curr_btc").value = data.last;
-            document.getElementById("min_buy").value = document.getElementById("curr_btc").value * 0.99025;
+            document.getElementById("curr_btc_usd").value = data.last;
+            min_buy = document.getElementById("curr_btc_usd").value * 0.99025;
+            document.getElementById("min_buy_usd").value = Math.floor(min_buy * 1e2) / 1e2;
             
-            load_nums(document.getElementById("btc_balance").value, document.getElementById("usd_balance").value)
-            callback();  
+            load_nums();
+        } else {
+            console.log('error')
+        }
+    }
+
+    request_eur.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response)
+        
+        if (request_eur.status >= 200 && request_eur.status < 400) {
+            var data = JSON.parse(this.response)
+            console.log("btc_price_eur")
+            console.log(data.last)
+            document.getElementById("curr_btc_eur").value = data.last;
+            min_buy = document.getElementById("curr_btc_eur").value * 0.99025;
+            document.getElementById("min_buy_eur").value = Math.floor(min_buy * 1e2) / 1e2;
+            
+            load_nums();
         } else {
             console.log('error')
         }
     }
     
     // Send request
-    for(var i = 0; i < 5; i++){
-        request.open('GET', 'https://www.bitstamp.net/api/v2/ticker/btceur/', true)
-        request.send();
-    }
+    request_usd.open('GET', 'https://www.bitstamp.net/api/ticker/', true)
+    request_usd.send();
+    request_eur.open('GET', 'https://www.bitstamp.net/api/v2/ticker/btceur/', true)
+    request_eur.send();
 }
 
-function load_nums(btc, usd) {
+function load_nums() {
     console.log("TEST");
-    console.log(btc);
-    console.log(usd);
-    var curr_btc = document.getElementById("curr_btc").value;
-    document.getElementById("btc_sell").value = Math.floor(btc * 0.995 * curr_btc * 1e2) / 1e2;
+    btc_balance = document.getElementById("btc_balance").value;
+    curr_btc_usd = document.getElementById("curr_btc_usd").value;
+    curr_btc_eur = document.getElementById("curr_btc_eur").value;
+    // console.log(btc);
+    // console.log(usd);
+    document.getElementById("btc_sell_usd").value = Math.floor(btc_balance * 0.995 * curr_btc_usd * 1e2) / 1e2;
+    document.getElementById("btc_sell_eur").value = Math.floor(btc_balance * 0.995 * curr_btc_eur * 1e2) / 1e2;
 }
 
 function add_row() {
@@ -52,12 +73,15 @@ function add_row() {
     col3.innerHTML = form2;
 }
 
-function calc_revenue(btc_balance, actual_money) {
+function calc_revenue() {
     var table = document.getElementById("per_calc_table");
     var op_amount = [];
     var op_buy_price = [];
     var op_fee = [];
     var op_btc = [];
+
+    btc_balance = document.getElementById("btc_balance").value;
+    btc_sell_eur = document.getElementById("curr_btc_eur").value;
 
     for (var i = 1; i < table.rows.length - 2; i++) {
         row = table.rows[i];
@@ -103,9 +127,9 @@ function calc_revenue(btc_balance, actual_money) {
     console.log(spent_sum);
 
     // var actual_btc = getElementById("btc_balance").value;
-    // var actual_money = getElementById("btc_sell").value;
+    // var btc_sell_eur = getElementById("btc_sell_eur").value;
 
-    roi_aux = ((actual_money / spent_sum) - 1) * 100;
+    roi_aux = ((btc_sell_eur / spent_sum)) * 100;
     btc_gain_aux = ((btc_balance / btc_sum) - 1) * 100;
 
     roi = Math.floor(roi_aux * 1e2) / 1e2;
